9819f70f901981a3f41cb5aad5583d4ce0dd0185
tsurumeso <nstm101339@gmail.com>
2017-07-10 22:42:58 +0900

    version 2.9

--- ImageScraper/MainForm.cs
+++ ImageScraper/MainForm.cs
@@ -7,7 +7,7 @@ namespace ImageScraper
 {
     public partial class MainForm : Form
     {
-        const string mVersionString = "2.8";
+        const string mVersionString = "2.9";
         private Downloader mDownloader;
         private Utilities.Logger mLogger;
         private Plugins.PluginInterface[] mPlugins;
